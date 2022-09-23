86cc64caf2e2c95ae32172ec1729589cb90b1f50
Rajko Radojkovic <radojkovic2020@gmail.com>
2022-09-23 22:09:05 +0200

    tesla_clone

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import './App.css';
+import Header from "./components/Header"
+import Home from "./components/Home"
+import Section from './components/Section';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
